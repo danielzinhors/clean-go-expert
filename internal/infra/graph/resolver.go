6930a8f2288b30791edc73db07702be59b3b8aa0
danielzinhors <danielzinhors@gmail.com>
2024-02-08 22:21:41 -0300

    init

---
+++ internal/infra/graph/resolver.go
@@ -0,0 +1,15 @@
+package graph
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+import "github.com/danielzinhors/clean-go-expert/internal/usecase"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	CreateOrderUseCase usecase.CreateOrderUseCase
+}
